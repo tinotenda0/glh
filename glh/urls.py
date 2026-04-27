781e223bd6cee27bd46b56fa31e22c03f7af83cb
Tino <tinotendamuzovaka5@gmail.com>
2026-04-27 10:53:43 +0100

    changes

--- glh/urls.py
+++ glh/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings, urls
+from django.views.static import serve
 from django.conf.urls.static import static
 
 
